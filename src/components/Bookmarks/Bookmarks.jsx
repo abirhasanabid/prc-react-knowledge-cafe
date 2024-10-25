33e05b2d3edff8e5be65584399010f4dbaf8f20c
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-10-25 16:49:56 +0600

    finally

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,11 +1,10 @@
 import Bookmark from "../Bookmark/Bookmark";
 
 const Bookmarks = ({ bookmarks, readTime }) => {
-    // console.log(readTime)
     return (
         <div className="md:w-1/3  bg-black/10 rounded-md rounded-md] ">
-            <div className="p-4 mt-3 mx-4">
-                <h1 className="text-red-700">Spent time on read :{readTime}</h1>
+            <div className="mx-4 mt-3 bg-purple-500 p-3 rounded-lg text-white">
+                <h1 className="font-semibold text-xl">Spent time on read :{readTime}</h1>
             </div>
             <h1 className="font-bold text-xl mx-4 mt-3"> Bookmarked Blogs : {bookmarks.length}</h1>
             {
